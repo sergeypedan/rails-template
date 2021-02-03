81d4fa02913796acb6efef8aa191a52615beca63
Sergey Pedan <sergey.pedan@gmail.com>
2021-02-03 20:34:46 +0700

    Updates

--- app/validators/json_validator.rb
+++ app/validators/json_validator.rb
@@ -3,8 +3,16 @@
 class JsonValidator < ActiveModel::EachValidator
 
   def validate_each(record, attribute, value)
-    return if value.blank? # presence validation should handle this
-    JSON.parse(value)
+    return if value.nil?
+    # return if value == "" no, because an empty String is not a valid JSON object
+
+    # This will blow up if JSON is invalid
+    parsed_value = JSON.parse! value
+
+    # Enforcing type of contained data
+    return unless type = options[:only]
+    record.errors.add attribute, :invalid_format unless parsed_value.is_a? type
+
   rescue JSON::ParserError
     record.errors.add attribute, :invalid_format
   end
