faac9138279c19f0be45901072802ff800a20d93
Sergey Pedan <sergey.pedan@gmail.com>
2021-07-27 19:50:31 +0700

    Updates Paperclip initializer

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -1,4 +1,5 @@
 # https://github.com/thoughtbot/paperclip/blob/master/lib/paperclip/attachment.rb#L13
+# https://www.rubydoc.info/gems/paperclip/Paperclip/Storage/S3
 
 options = Paperclip::Attachment.default_options
 
@@ -8,27 +9,16 @@ options[:url]     = "/uploads/:class/:id/:style.:extension"
 options[:path]    = ":rails_root/public:url"
 
 # AWS
-options[:storage] = :s3
-options[:s3_credentials] = {
-  bucket: ENV['AWS_BUCKET'],
-  access_key_id: ENV["AWS_KEY_ID"],
-  secret_access_key: ENV["AWS_ACCESS_KEY"],
-  s3_region: ENV['AWS_REGION']
-}
-options[:path] = "#{Rails.env}/:class/:id/:style.:extension"
-options[:url] = ":s3_domain_url"
-
-# ---
-
-options = Paperclip::Attachment.default_options
-
-options[:path]           = "#{Rails.env}/:class/:id/:style.:extension"
-options[:storage]        = :s3
-options[:url]            = ":s3_domain_url"
+options[:bucket]         = Rails.application.credentials.dig(:aws, :s3, :bucket)
 # options[:s3_host_name] = 's3-us-west-2.amazonaws.com'
+options[:path]           = "#{Rails.env}/:class/:id/:style.:extension"
 options[:s3_credentials] = {
   access_key_id:     Rails.application.credentials.dig(:aws, :s3, :access_key_id),
-  bucket:            Rails.application.credentials.dig(:aws, :s3, :bucket),
-  s3_region:         Rails.application.credentials.dig(:aws, :s3, :region),
   secret_access_key: Rails.application.credentials.dig(:aws, :s3, :secret_access_key)
 }
+# options[:s3_host_name]   = 's3-us-west-2.amazonaws.com'
+options[:s3_protocol] = :https
+options[:s3_region]   = Rails.application.credentials.dig(:aws, :s3, :region)
+options[:s3_storage_class] = :REDUCED_REDUNDANCY
+options[:storage]     = :s3
+options[:url]         = ":s3_domain_url"
